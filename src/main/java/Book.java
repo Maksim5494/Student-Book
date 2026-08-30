8f6005e81d1fb15ad7c8889f35a0bb2caf9b4c29
Maksim <52542282+Maksim5494@users.noreply.github.com>
2026-08-30 16:26:58 +0500

    Refactor Book class to include author and adjust methods
    Updated the Book class to include an author field, modified the constructor, and adjusted the equals, hashCode, compareTo, and toString methods accordingly.

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,54 +1,50 @@
 import java.util.Objects;
 
 public class Book implements Comparable<Book> {
-
     private final String title;
-    private final int pages;
+    private final String author;
     private final int year;
+    private final int pages;
 
-    public Book(String title, int pages, int year) {
+    public Book(String title, String author, int year, int pages) {
         this.title = title;
-        this.pages = pages;
+        this.author = author;
         this.year = year;
+        this.pages = pages;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    @Override
-    public int compareTo(Book other) {
-        return Integer.compare(this.pages, other.pages);
-    }
+    public int getYear() { return year; }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Book book)) return false;
-        return pages == book.pages
-                && year == book.year
-                && Objects.equals(title, book.title);
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && 
+               pages == book.pages && 
+               Objects.equals(title, book.title) && 
+               Objects.equals(author, book.author);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, pages, year);
+        return Objects.hash(title, author, year, pages);
+    }
+
+    @Override
+    public int compareTo(Book other) {
+        // Сравнение по всем полям для согласованности с equals
+        int res = this.title.compareTo(other.title);
+        if (res != 0) return res;
+        res = this.author.compareTo(other.author);
+        if (res != 0) return res;
+        res = Integer.compare(this.year, other.year);
+        if (res != 0) return res;
+        return Integer.compare(this.pages, other.pages);
     }
 
     @Override
     public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", pages=" + pages +
-                ", year=" + year +
-                '}';
+        return String.format("Книга '%s' (%s, %d г., %d стр.)", title, author, year, pages);
     }
 }

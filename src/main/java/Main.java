790b97c5019c30a58d1445c22e5d40da34db0292
Maksim <52542282+Maksim5494@users.noreply.github.com>
2026-08-30 16:27:57 +0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,26 @@
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        List<Student> students = readStudentsFromFile("src/main/resources/students.txt");
-
-        students.stream()
+        StudentFileReader.read(Path.of("students.txt"))
+                .stream()
                 .peek(System.out::println)
-                .map(Student::getBooks)
-                .flatMap(List::stream)
-                .sorted()
+                .flatMap(student -> student.getBooks().stream())
                 .distinct()
                 .filter(book -> book.getYear() > 2000)
+                .sorted()
                 .limit(3)
                 .map(Book::getYear)
-                .findFirst()
+                .findAny()
                 .ifPresentOrElse(
-                        year -> System.out.println("Год выпуска найденной книги: " + year),
-                        () -> System.out.println("Такая книга отсутствует")
+                        year -> System.out.println(
+                                "Год выпуска найденной книги: " + year
+                        ),
+                        () -> System.out.println(
+                                "Книга, выпущенная после 2000 года, отсутствует"
+                        )
                 );
-    }
-
-    private static List<Student> readStudentsFromFile(String filePath) throws IOException {
-        return Files.readAllLines(Path.of(filePath))
-                .stream()
-                .map(line -> line.split(";"))
-                .map(parts -> new Student(
-                        parts[0],
-                        Arrays.stream(parts[1].split("\\|"))
-                                .map(bookData -> bookData.split(","))
-                                .map(bookParts -> new Book(
-                                        bookParts[0],
-                                        Integer.parseInt(bookParts[1]),
-                                        Integer.parseInt(bookParts[2])
-                                ))
-                                .toList()
-                ))
-                .toList();
     }
 }
